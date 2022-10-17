97531b650fcaeb1ec08c7e2e168029449dc43898
Igor Sfiligoi <isfiligoi@sdsc.edu>
2022-10-16 19:50:30 -0700

    Tune Unweighted step size

--- src/unifrac_task.hpp
+++ src/unifrac_task.hpp
@@ -402,8 +402,12 @@ namespace SUCMP_NM {
     template<class TFloat>
     class UnifracUnweightedTask : public UnifracTask<TFloat,uint64_t> {
       public:
-        // use the smaller size for historical reasons
-        static const unsigned int step_size = 16*4/sizeof(TFloat);
+#ifdef _OPENACC
+        static const unsigned int step_size = 64*4/sizeof(TFloat);
+#else
+        // CPUS like a smaller step_size
+        static const unsigned int step_size = 64*4/sizeof(TFloat);
+#endif
 
         static const unsigned int RECOMMENDED_MAX_EMBS = UnifracTask<TFloat,uint64_t>::RECOMMENDED_MAX_EMBS_BOOL;
 
