f9d2c1c8f3b4f5121f947d8cd431e4aef47fa8d9
Igor Sfiligoi <isfiligoi@sdsc.edu>
2025-12-03 14:19:59 -0800

    Update NV HPC SDK version

--- scripts/install_hpc_sdk.sh
+++ scripts/install_hpc_sdk.sh
@@ -36,9 +36,9 @@ export PATH=$PWD/conda_nv_bins:$PATH
 # Install the NVIDIA HPC SDK
 
 # This link may need to be updated, as new compiler versions are released
-# Note: Verified that it works with v25.1
+# Note: Verified that it works with v25.11
 if [ "x${NV_URL}" == "x" ]; then
-  NV_URL=https://developer.download.nvidia.com/hpc-sdk/25.1/nvhpc_2025_251_Linux_x86_64_cuda_multi.tar.gz
+  NV_URL=https://developer.download.nvidia.com/hpc-sdk/25.11/nvhpc_2025_2511_Linux_x86_64_cuda_multi.tar.gz
 fi
 
 echo "Downloading the NVIDIA HPC SDK"
